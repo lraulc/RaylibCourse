15b6f1706bdc3f3334a523b1a4646b43629f134b
GrayscaleInteractive <info@grayscaleinteractive.com>
2023-01-06 14:24:15 -0600

    collision detection

--- Axe/main.cpp
+++ Axe/main.cpp
@@ -30,12 +30,25 @@ int main()
     // Circle Init Positions
     float circle_x = window.x / 2;
     float circle_y = window.y / 2;
+    float circle_radius = 25;
+    //  Circle Edges
+    float l_circle_x = circle_x - circle_radius; // Left distance
+    float r_circle_x = circle_x + circle_radius; // Right Distance
+    float u_circle_y = circle_y - circle_radius; // Upper Distance
+    float b_circle_y = circle_y + circle_radius; // Bottom Distance
 
     // Rectangle Variables
     float rect_x = 300;
     float rect_y = 0;
+    float rect_length = 50;
     int direction = 10; // Will move downwards
     Color rect_color = {172, 200, 123, 255};
+
+    // Rectangle Edges
+    float l_rect_x = rect_x;
+    float r_rect_x = rect_x + rect_length;
+    float u_rect_y = rect_y;
+    float b_rect_y = rect_y + rect_length;
 
     while (isRunning)
     {
@@ -47,7 +60,7 @@ int main()
             // Draw Start
 
             /*********** Draw Cricle Start ****************/
-            DrawCircle(circle_x, circle_y, 25, BLUE);
+            DrawCircle(circle_x, circle_y, circle_radius, BLUE);
 
             if ((IsKeyDown(KEY_A) || IsKeyDown(KEY_LEFT)) && (circle_x >= 0))
             {
@@ -68,7 +81,7 @@ int main()
             /*********** Draw Cricle END ****************/
 
             /*********** Draw and Move Square START ****************/
-            DrawRectangle(rect_x, rect_y, 50, 50, rect_color);
+            DrawRectangle(rect_x, rect_y, rect_length, rect_length, rect_color);
             rect_y += direction;
 
             if ((rect_y >= window.y) || (rect_y <= 0 - 25))
@@ -77,6 +90,10 @@ int main()
             }
 
             /*********** Draw Square END ****************/
+
+            /*********** Detect Collision Start ***************/
+
+            /*********** Detect Collision End ***************/
 
             // Window Should close
             // Clear after finished drawing
